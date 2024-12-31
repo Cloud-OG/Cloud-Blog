85da18313b2c4e4846fcc15921d67d7f1d82c242
Cloud <1621454829@qq.com>
2024-12-31 17:18:55 +0800

    Update SocialButton.js

--- themes/hexo/components/SocialButton.js
+++ themes/hexo/components/SocialButton.js
@@ -23,6 +23,7 @@ const SocialButton = () => {
   const CONTACT_XIAOHONGSHU = siteConfig('CONTACT_XIAOHONGSHU')
   const CONTACT_ZHISHIXINGQIU = siteConfig('CONTACT_ZHISHIXINGQIU')
   const CONTACT_WEHCHAT_PUBLIC = siteConfig('CONTACT_WEHCHAT_PUBLIC')
+  const CONTACT_QQ = siteConfig('CONTACT_QQ')
 
   const [qrCodeShow, setQrCodeShow] = useState(false)
 
@@ -175,6 +176,15 @@ const SocialButton = () => {
               </div>
             </div>
           </button>
+        )}
+        {CONTACT_QQ && (
+          <a
+            target='_blank'
+            rel='noreferrer'
+            title={'qq'}
+            href={CONTACT_QQ}>
+            <i className='transform hover:scale-125 duration-150 fab fa-qq dark:hover:text-indigo-400 hover:text-indigo-600' />
+          </a>
         )}
       </div>
     </div>
